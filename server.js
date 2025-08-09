11775a1a51f5db7d789e6d733eac1e3f79b30e1a
fengcheng0126 <fengcheng0126@gmail.com>
2025-08-09 15:27:14 -0500

    removed static serve causing error

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY, {
 app.use(express.json());
 
 // Serve static files (e.g. index.html)
-app.use(express.static(process.env.STATIC_DIR));
+// app.use(express.static(process.env.STATIC_DIR));
 
-app.get("/", (req, res) => {
-  const path = resolve(process.env.STATIC_DIR + "/index.html");
-  res.sendFile(path);
-});
+// app.get("/", (req, res) => {
+//   const path = resolve(process.env.STATIC_DIR + "/index.html");
+//   res.sendFile(path);
+// });
 
 app.get("/config", (req, res) => {
   res.send({
